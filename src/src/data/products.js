1e48697956572552637cefa7fef28575ec560fdc
ANASSRTGD <118916163+ANASSRTGD@users.noreply.github.com>
2026-01-06 00:13:09 +0100

    Delete src/src/data directory

--- src/src/data/products.js
+++
@@ -1,8 +0,0 @@
-// src/data/products.js
-const products = [
-  { id: 1, name: 'تيشيرت قطني', price: 120, description: 'تيشيرت قطني عالي الجودة' },
-  { id: 2, name: 'فستان صيفي', price: 250, description: 'فستان أنيق ومريح' },
-  { id: 3, name: 'بلوزة كلاسيكية', price: 150, description: 'بلوزة قطنية رائعة' },
-];
-
-export default products;
