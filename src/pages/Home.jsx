10b4050aeb424db226efa9577ad55d1fc9cc565d
ANASSRTGD <118916163+ANASSRTGD@users.noreply.github.com>
2026-01-06 00:15:09 +0100

    Fix import statement formatting in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import products from '../data/products'
+import products from '../data/products';
 import ProductCard from '../components/ProductCard'
 
 export default function Home() {
